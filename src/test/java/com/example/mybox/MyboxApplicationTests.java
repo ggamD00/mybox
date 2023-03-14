452ce5feb22556a61713884913854596fd9f164a
ggamD00 <jaehuek@naver.com>
2023-03-14 15:40:26 +0900

    Initial commit

---
+++ src/test/java/com/example/mybox/MyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
